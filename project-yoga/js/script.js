40c024fd42eb7ab0b209b667e1d8e038c693d969
Mihail <mikrul@rambler.ru>
2019-05-07 02:38:02 +0300

    My tenth commit

--- project-yoga/js/script.js
+++ project-yoga/js/script.js
@@ -93,18 +93,6 @@ window.addEventListener('DOMContentLoaded', function() {
         close = document.querySelector('.popup-close'),
         descrBtns = document.querySelectorAll('.description-btn');
 
-    // more.addEventListener('click', function() {
-    //     overlay.style.display = 'block';
-    //     this.classList.add('more-splash');
-    //     document.body.style.overflow = 'hidden';
-    // });
-
-    // close.addEventListener('click', function() {
-    //     overlay.style.display = 'none';
-    //     more.classList.remove('more-splash');
-    //     document.body.style.overflow = '';
-    // });
-
     function openModal(a) {
         a.addEventListener('click', function() {
             overlay.style.display = 'block';
@@ -145,5 +133,63 @@ window.addEventListener('DOMContentLoaded', function() {
     
     const item = new Options(300, 350, "red", 14, "center");
     
-    item.createDiv();
+    // item.createDiv();
+
+    // Form
+
+    let message = {
+        loading: "Загрузка...",
+        sucsess: "Спасибо! Скоро мы с Вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+     
+    let form = document.querySelector('.main-form'),
+        formDown = document.querySelector('#form'),
+        input = form.getElementsByTagName ('input'),
+        statusMessage = document.createElement('div');
+         
+     
+        statusMessage.classList.add('status');
+     
+    function sendForm(elem){
+     
+        elem.addEventListener('submit', function (event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+        
+            let formData = new FormData(elem);
+        
+        function postData() {
+            return new Promise(function (resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                request.onreadystatechange = function () {
+                    if(request.readyState < 4) {
+                        resolve();
+                    } else if (request.readyState === 4) {
+                        if (request.status == 200 && request.status < 300) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }
+                };
+                request.send(formData);
+            });
+        }
+            function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+        postData (formData)
+            .then (() => statusMessage.innerHTML = message.loading)
+            .then (() => statusMessage.innerHTML = message.sucsess)
+            .catch (() => statusMessage.innerHTML = message.failure)
+            .then (clearInput);
+        });
+    }
+    sendForm(form);
+    sendForm(formDown);
 });
